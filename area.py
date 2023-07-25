a250f9a805787d2b3aea743f114775b739023927
Zoozu2 <shreyasvartak2010@gmail.com>
2023-07-25 23:15:59 +0530

    Initial commit

---
+++ area.py
@@ -0,0 +1,10 @@
+# #Testing
+# name = input("What is your name? ")
+# age = int(input("How old are you? "))
+# hello = "Hello"
+# print(hello, name , age) 
+
+# Calc area of a circle
+radius = input('Enter the radius of your circle (m): ')
+area = 3.142 * int(radius) **2
+print('The area of your circle is:', area)
