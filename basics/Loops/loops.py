16b4b5936d21ea776f7a229bd2e85d2363392c98
Zoozu2 <shreyasvartak2010@gmail.com>
2023-10-23 17:36:18 +0530

    For Loops v2

--- basics/Loops/loops.py
+++ basics/Loops/loops.py
@@ -3,3 +3,12 @@ bros = ["shreyas", "tejas", "preshit",'anish','siddhesh','sahil']
 # for bro in bros[0:4]:
 #     print(bro)
 # Output: shreyas tejas preshit anish
+
+# Assigning and check if a particular variable is in the code
+for bro in bros:
+    if bro == "anish":
+        print(f'{bro} - is my best friend')
+    else:
+        print(bro)
+        
+# output: shreyas tejas preshit anish - is my best friend siddhesh sahil
