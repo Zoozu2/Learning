3554d30395f4f47b66829ff1b17a3a4a01f781ef
Zoozu2 <shreyasvartak2010@gmail.com>
2023-10-23 23:57:50 +0530

    While_Loops
    I learned While Loops today and added if statements to it to make it more complex.😂

---
+++ basics/Loops/while_loops.py
@@ -0,0 +1,33 @@
+age = 24
+num = 0
+count = 0
+
+
+while num < age:
+    if num == 2:
+        num+=1
+        continue
+    if num % 2 == 0:
+        print(num)
+        count += 1
+    if num>10:
+        break
+    
+    num += 1
+
+print(f'Final number: {num}')
+print(f'Final count: {count}')
+
+# Explaination of the above code:
+# We have a variable called age which is 24
+# We have a variable called num which is 0
+# We have a variable called count which is 0
+#
+# While num is less than age, do the following:
+# If num is equal to 2, then add 1 to num and continue
+# If num is divisible by 2, then print num and add 1 to count
+# If num is greater than 10, then break
+# Add 1 to num
+#
+# Print the final number
+# Print the final count
